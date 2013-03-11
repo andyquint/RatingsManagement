aeb61a83f0ade0e1f63fc19b7dcb641e79e1b418
Andrew Quintanilla <quint.andrew@gmail.com>
2013-03-11 11:57:55 -0700

    Added functions for Create and Delete
    Added functions to Create and Delete databases. In the middle of
    creating a function to insert into a table. For now, every attribute is
    a string. Anything in the main function is for testing purposes only.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Andrew Quintanilla
 # CPSC254
 # Manages Ratings Databases
-import glob, sqlite3
+import os, glob, sqlite3
 
 # If no databases, return false.
 # If there are databases, return list of database names
@@ -11,14 +11,40 @@ def CheckDatabases():
 		return False
 	else:
 		return databaselist
-		
-# Name: Database name (no spaces, special characters, is used to name database file
-# Categories: List of categories for database
+
+# Assume all potential inputs are sanitized		
+# Name (string): Database name (no spaces, special characters, is used to name database file
+# Categories (list of strings): List of categories for database
 def CreateNewDatabase(name, categories):
 	conn = sqlite3.connect(name + '.db')
+	c = conn.cursor()
+	
+	# Constructs CREATE TABLE command
+	newtable = 'CREATE TABLE '
+	newtable += name+' ('
+	for i in range(len(categories)):
+		newtable += categories[i]
+		newtable += ' text'
+		if i != len(categories)-1:
+			newtable += ', '
+	
+	conn.commit()
+	conn.close()
+
+# name: Database to be deleted
+def DeleteDatabase(name):
+	os.remove(name+'.db')
+
+# 
+def InsertIntoTable(name
 	
 def main():
-	
+	CreateNewDatabase('test', ['cat1', 'cat2', 'cat3'])
+	l = CheckDatabases()
+	print(l)
+	DeleteDatabase('test')
+	l=CheckDatabases()
+	print(l)
 
 if __name__ == '__main__':
 	main()
