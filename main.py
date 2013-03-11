30eaf13a8005d4bfbfaa669556cac4217acf988b
Andrew Quintanilla <quint.andrew@gmail.com>
2013-03-11 12:56:27 -0700

    Updated some functions
    Updated CheckDatabase, CheckTableNamed, CreateTable, DropTable to work
    properly.
    Still need to finish Insert.

--- main.py
+++ main.py
@@ -2,21 +2,33 @@
 # CPSC254
 # Manages Ratings Databases
 import os, glob, sqlite3
+ratingsDB = 'ratings.db'
 
-# If no databases, return false.
-# If there are databases, return list of database names
+# If database exists, returns True, otherwise, returns False.
 def CheckDatabase():
-	database = glob.glob('ratings.db')
+	database = glob.glob(ratingsDB)
 	if (not database):
 		return False
 	else:
 		return True
+		
+# Checks if there exists a table matching name. Returns True if so, False otherwise.
+def CheckTableNamed(name):
+	conn = sqlite3.connect(ratingsDB)
+	c = conn.cursor()
+	c.execute("SELECT name FROM sqlite_master WHERE name='%s'" % name)
+	nameslist = c.fetchall()
+	conn.close()
+	if nameslist:
+		return True
+	else:
+		return False
 
 # Assume all potential inputs are sanitized		
 # Name (string): Table name (no spaces or special characters, is used to name table
 # Categories (list of strings): List of categories for table
 def CreateTable(name, categories):
-	conn = sqlite3.connect(name + '.db')
+	conn = sqlite3.connect(ratingsDB)
 	c = conn.cursor()
 	
 	# Constructs CREATE TABLE command
@@ -27,30 +39,36 @@ def CreateTable(name, categories):
 		newtable += ' text'
 		if i != len(categories)-1:
 			newtable += ', '
-	
+		else:
+			newtable += ')'
+	c.execute(newtable)
 	conn.commit()
 	conn.close()
 
 # name: Database to be deleted
 def DropTable(name):
-	os.remove(name+'.db')
+	conn = sqlite3.connect(ratingsDB)
+	c = conn.cursor()
+	c.execute('DROP TABLE IF EXISTS %s' % name)
+	conn.commit()
+	conn.close()
 
 # name: name of database to be inserted into
 # tuple: list of attributes of new item
 def Insert(name, tuple):
-	conn = sqlite3.connect(name+'.db')
+	conn = sqlite3.connect(ratingsDB)
 	c = conn.cursor()
 	
 	# Constructs INSERT INTO command
-	insertinto = 'INSERT INTO ' + 
+	insertinto = 'INSERT INTO '
 	
 def main():
-	CreateTable('test', ['cat1', 'cat2', 'cat3'])
-	l = CheckDatabases()
-	print(l)
-	DeleteDatabase('test')
-	l=CheckDatabases()
-	print(l)
+	DropTable('test')
+	DropTable('test1')
+	CreateTable('test',['cat1','cat2','cat3'])
+	CreateTable('test1',['cat1','cat2'])
+	print(CheckTableNamed('test'))
+	print(CheckTableNamed('test2')) 
 
 if __name__ == '__main__':
 	main()
