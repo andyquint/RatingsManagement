c746c033161b55148d3ba6dd7c112a4315067fef
Andrew Quintanilla <quint.andrew@gmail.com>
2013-03-15 03:42:56 -0700

    Working on DisplayMenu

--- cli.py
+++ cli.py
@@ -3,31 +3,47 @@
 # Command Line Interface for Ratings Manager
 import sys, dbmanager
 
-# State Definitions
-# 0: Create New List / Select Existing List
-state = 0
-
 def DisplayWelcome():
 	print('Ratings Manager\n')
-	
-def DisplayMenu():
-	if state == 0:
-		print('1. Create New List')
-		print('2. Select Existing List')
-		print('3. Quit')
-		opt = GetOption()
-		if opt == 1:
-			state = 1
-		elif opt == 2:
-			state = 2
-		elif opt == 3:
-			Quit()
-		else:
-			InvalidOption()
+
+class CLI:
+# State Definitions
+# 0: Create New List / Select Existing List
+# 1: Create New List
+# 2: Select Existing List
+	def __init__(self):
+		self.state = 0
 		
-def GetOption():
-	print('>> ')
-	option = input('>> ')
+	def DisplayMenu(self):
+		if self.state == 0:
+			print('1. Create New List')
+			print('2. Select Existing List')
+			print('3. Quit')
+			opt = int(GetOption(1,3))
+			if opt == 1:
+				self.state = 1
+			elif opt == 2:
+				self.state = 2
+			elif opt == 3:
+				Quit()
+			else:
+				InvalidOption()
+		elif self.state == 2:
+			listnames = dbmanager.ListTableNames()
+			for n in range(len(listnames)):
+				print('%i: %s' % (n+1,listnames[n]))
+			print('%i: Quit' % (len(listnames)+1))
+			
+		else:
+			Quit()
+
+# min: Minimum acceptable input number
+# max: Maximum acceptable input number			
+def GetOption(min, max):
+	option = int(input('>> '))
+	while (option < min or option > max):
+		InvalidOption()
+		option = int(input('>> '))
 	return option
 	
 def InvalidOption():
@@ -37,9 +53,10 @@ def Quit():
 	sys.exit()
 	
 def main():
-	DisplayWelcome():
+	cli = CLI()
+	DisplayWelcome()
 	while(True):
-		DisplayMenu()
+		cli.DisplayMenu()
 		
 
 if __name__ == '__main__':
